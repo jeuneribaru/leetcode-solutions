13607fdaa6c6a6d81be3815021b3cb2532997305
Julien PONS <159694393+jeuneribaru@users.noreply.github.com>
2026-07-24 16:58:13 +0200

    ✅ Add Two Numbers | Medium | Python | Runtime: 0 ms | Memory: 19.2 MB | 2026-07-24

--- medium/0002-add-two-numbers/solution.py
+++ medium/0002-add-two-numbers/solution.py
@@ -1,20 +1,32 @@
-        result = int(s1) + int(s2)
-            l2 = l2.next
-        s_result = str(result)[::-1]
-        factice = ListNode(0)
-        while l2.next != None :
-            s2 += str(l2.val)
-            l1 = l1.next
-            s1 += str(l1.val)
-        while l1.next != None :
-        s2 = ""
-        s1 = ''
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        print(s1, s2,result)
-        noeud_actif = factice
-        for cara in s_result : 
-            noeud = ListNode(int(cara))
-        s1 += str(l1.val)
-        s2 += str(l2.val)
-            noeud_actif.next = noeud
-            noeud_actif = noeud 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        s1 = ''
+        s2 = ""
+        while l1.next != None :
+            s1 += str(l1.val)
+            l1 = l1.next
+        s1 += str(l1.val)
+        while l2.next != None :
+            s2 += str(l2.val)
+            l2 = l2.next
+        s2 += str(l2.val)
+        result = int(s1) + int(s2)
+        print(s1, s2,result)
+        s_result = str(result)[::-1]
+        factice = ListNode(0)
+        noeud_actif = factice
+        for cara in s_result : 
+            noeud = ListNode(int(cara))
+            noeud_actif.next = noeud
+            noeud_actif = noeud 
+        return factice.next
+
+
+
+
+        
