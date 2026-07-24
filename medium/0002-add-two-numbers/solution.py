423564897432e6bcf071ae2b3ec7b1051f66b86c
Julien PONS <159694393+jeuneribaru@users.noreply.github.com>
2026-07-24 16:47:12 +0200

    ✅ Add Two Numbers | Medium | Python | Runtime: 0 ms | Memory: 19.5 MB | 2026-07-24

---
+++ medium/0002-add-two-numbers/solution.py
@@ -0,0 +1,20 @@
+        result = int(s1[::-1]) + int(s2[::-1])
+            l2 = l2.next
+        s_result = str(result)[::-1]
+        factice = ListNode(0)
+        noeud_actif = factice
+        for cara in s_result : 
+            noeud = ListNode(cara)
+            noeud_actif.next = noeud
+            noeud_actif = noeud 
+        return factice.next
+
+
+        while l2.next != None :
+            s2 += str(l2.val)
+            l1 = l1.next
+            s1 += str(l1.val)
+        while l1.next != None :
+        s2 = ""
+        s1 = ''
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
