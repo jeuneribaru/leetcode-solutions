1e91837590886ae467f34fe135f4121f1d131c6d
Julien PONS <159694393+jeuneribaru@users.noreply.github.com>
2026-07-24 19:23:29 +0200

    ✅ Valid Parentheses | Easy | C | Runtime: 0 ms (100.00%) | Memory: 8.8 MB | 2026-07-24

--- easy/0020-valid-parentheses/solution.c
+++ easy/0020-valid-parentheses/solution.c
@@ -1,31 +1,31 @@
-        } 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-        else {
+bool isValid(char* s) {
+    int len = strlen(s);
+    char stack[len]; 
+    int top = -1; 
 
-            if (top == -1) return false;
-            
-            if (c == ')' && stack[top] != '(') return false;
-            if (c == '}' && stack[top] != '{') return false;
-            if (c == ']' && stack[top] != '[') return false;
-            
+    for (int i = 0; i < len; i++) {
+        char c = s[i];
 
-            top--;
-        }
-    }
+        if (c == '(' || c == '{' || c == '[') {
+            stack[++top] = c;
+        } 
 
-    return top == -1;
+        else {
 
-bool isValid(char* s) {
-    int len = strlen(s);
-    char stack[len]; 
-    int top = -1; 
+            if (top == -1) return false;
+            
+            if (c == ')' && stack[top] != '(') return false;
+            if (c == '}' && stack[top] != '{') return false;
+            if (c == ']' && stack[top] != '[') return false;
+            
 
-    for (int i = 0; i < len; i++) {
-        char c = s[i];
+            top--;
+        }
+    }
 
-        if (c == '(' || c == '{' || c == '[') {
-            stack[++top] = c;
-#include <string.h>
-#include <stdbool.h>
+    return top == -1;
 }
-#include <stdio.h>
