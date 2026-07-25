58c3bcc036975bf66e1f1749d698e55e4ca8f960
Julien PONS <159694393+jeuneribaru@users.noreply.github.com>
2026-07-25 13:28:07 +0200

    ✅ Roman to Integer | Easy | Python | Runtime: 3 ms (80.03%) | Memory: 19.3 MB | 2026-07-25

--- easy/0013-roman-to-integer/solution.py
+++ easy/0013-roman-to-integer/solution.py
@@ -1,24 +1,23 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        dic = {
-            'I' : 1, 
-            'V' : 5, 
-            'X' : 10, 
-            'L': 50, 
-            'C': 100, 
-            'D' : 500, 
-            'M' : 1000
-        }
-        for i in range(len(s)): 
-        ret = 0
-            ret += dic[s[i]] 
-        for j in range(1,len(s)) :
-            if s[j-1] == "I" and (s[j] == "V" or s[j] == "X"):
-                    ret -= 2
-
-            elif s[j-1] == "X" and (s[j] == "L" or s[j] == "C"):
-                    ret -= 20
-            elif s[j-1] == "C" and (s[j] == "D" or s[j] == "M"):
-                    ret -= 200
-        return ret 
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic = {
+            'I' : 1, 
+            'V' : 5, 
+            'X' : 10, 
+            'L': 50, 
+            'C': 100, 
+            'D' : 500, 
+            'M' : 1000
+        }
+        ret = 0
+        for i in range(len(s)): 
+            ret += dic[s[i]] 
+        for j in range(1,len(s)) :
+            if s[j-1] == "I" and (s[j] == "V" or s[j] == "X"):
+                    ret -= 2
+            elif s[j-1] == "X" and (s[j] == "L" or s[j] == "C"):
+                    ret -= 20
+            elif s[j-1] == "C" and (s[j] == "D" or s[j] == "M"):
+                    ret -= 200
+        return ret 
 
