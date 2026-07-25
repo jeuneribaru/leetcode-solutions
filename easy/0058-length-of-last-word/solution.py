a9e6ff2f1389033890220ea7f185a2a5218a98fd
Julien PONS <159694393+jeuneribaru@users.noreply.github.com>
2026-07-25 12:03:56 +0200

    ✅ Length of Last Word | Easy | Python | Runtime: 0 ms | Memory: 19.3 MB | 2026-07-25

--- easy/0058-length-of-last-word/solution.py
+++ easy/0058-length-of-last-word/solution.py
@@ -3,9 +3,11 @@ class Solution:
         i = len(s)-1
         while i > 0 and s[i] != " " :
             i -= 1
-        ret = -1
+        ret = 0
         for j in range(i, len(s)) :
-            ret += 1
+                ret += 1
 
         return ret 
+            if s[j] != " " :
+        s.strip()
 
