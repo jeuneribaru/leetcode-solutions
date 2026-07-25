7ee2935053032ad332ef7f2c34894b3596def4b8
Julien PONS <159694393+jeuneribaru@users.noreply.github.com>
2026-07-25 12:02:46 +0200

    ✅ Length of Last Word | Easy | Python | Runtime: 0 ms | Memory: 19.3 MB | 2026-07-25

--- easy/0058-length-of-last-word/solution.py
+++ easy/0058-length-of-last-word/solution.py
@@ -3,7 +3,7 @@ class Solution:
         i = len(s)-1
         while i > 0 and s[i] != " " :
             i -= 1
-        ret = 0
+        ret = -1
         for j in range(i, len(s)) :
             ret += 1
 
