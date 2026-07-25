dd1550f54b2cd0c8cf59e04273ce0b843002269b
Julien PONS <159694393+jeuneribaru@users.noreply.github.com>
2026-07-25 12:54:16 +0200

    ✅ Maximum Product of Two Digits | Easy | Python | Runtime: 0 ms | Memory: 19.2 MB | 2026-07-25

--- easy/3536-maximum-product-of-two-digits/solution.py
+++ easy/3536-maximum-product-of-two-digits/solution.py
@@ -1,15 +1,15 @@
-class Solution:
-    def maxProduct(self, n: int) -> int:
-        str_n = str(n)
-        if len(str_n) == 2 :
-            return int(str_n[0])*int(str_n[1])
-        else: 
-            maxx = 0
-            for i in range(0, len(str_n)) : 
-                for j in range(i+1, len(str_n)) :
-                pro = int(str_n[i])
-                    pro *= int(str_n[j])
-                if pro > maxx :
-                    maxx = pro 
-            return maxx
-                    
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        str_n = str(n)
+        if len(str_n) == 2 :
+            return int(str_n[0])*int(str_n[1])
+        else: 
+            maxx = 0
+            for i in range(0, len(str_n)) : 
+                pro = int(str_n[i])
+                for j in range(i+1, len(str_n)) :
+                    pro *= int(str_n[j])
+                if pro > maxx :
+                    maxx = pro 
+            return maxx
+                    
